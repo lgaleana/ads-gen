37de6872a4b593c63998e91606e366a860d6d388
lgaleana-LLM <lsgaleana+llm@gmail.com>
2024-04-26 19:00:43 +0000

    AG-7: Subtask - Python script that extracts all text from an url

--- scripts/url_text_extractor.py
+++ scripts/url_text_extractor.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 def extract_text_from_url(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+    for br in soup.find_all("br"):
+        br.replace_with(" ")
     text = soup.get_text()
     cleaned_text = ' '.join(text.split())
     print(cleaned_text)

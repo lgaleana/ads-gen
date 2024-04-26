5b56ae308ff4bf7de78da4c0d1b0557a6c384169
lgaleana-LLM <lsgaleana+llm@gmail.com>
2024-04-26 19:21:46 +0000

    AG-8: Subtask - Extract all images from URL (Amended)

--- tests/test_url_text_extractor.py
+++ tests/test_url_text_extractor.py
@@ -1,19 +1,14 @@
 import pytest
-from scripts.url_text_extractor import extract_text_from_url, extract_images_from_url
+from bs4 import BeautifulSoup
+from scripts.url_text_extractor import extract_text_from_soup, extract_images_from_soup
 from unittest.mock import patch
 
 
-def test_extract_text_from_url():
-    url = 'http://example.com'
-    with patch('requests.get') as mocked_get:
-        mocked_get.return_value.text = '<html><body>Example Domain</body></html>'
-        extract_text_from_url(url)
-        mocked_get.assert_called_with(url)
+def test_extract_text_from_soup():
+    soup = BeautifulSoup('<html><body>Example Domain</body></html>', 'html.parser')
+    extract_text_from_soup(soup)
 
 
-def test_extract_images_from_url():
-    url = 'http://example.com'
-    with patch('requests.get') as mocked_get:
-        mocked_get.return_value.text = '<html><body><img src="example.jpg"></body></html>'
-        extract_images_from_url(url)
-        mocked_get.assert_called_with(url)
+def test_extract_images_from_soup():
+    soup = BeautifulSoup('<html><body><img src="example.jpg"></body></html>', 'html.parser')
+    extract_images_from_soup(soup)
